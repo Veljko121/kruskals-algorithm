3fb49b19d259ab5c2edd96f74d255bb3a6dda6dd
Veljko Nikolić <nikolicveljko01@gmail.com>
2026-02-02 14:50:10 +0100

    Refactored utils

--- utils.hpp
+++ utils.hpp
@@ -10,14 +10,7 @@
 
 using namespace std;
 
-void print_mst(vector<Edge> mst) {
-    cout << "[";
-    for (int i = 0; i < mst.size(); i++) {
-        cout << "(" << mst[i].u << ", " << mst[i].v << ", " << mst[i].weight << ")";
-        if (i < mst.size() - 1) cout << ", ";
-    }
-    cout << "]" << endl;
-}
+Graph generate_graph(unsigned num_of_vertices, unsigned num_of_edges);
 
 Graph generate_graph(unsigned num_of_vertices, unsigned num_of_edges) {
     // these checks ensure that dimensions are valid
@@ -25,19 +18,21 @@ Graph generate_graph(unsigned num_of_vertices, unsigned num_of_edges) {
     if (num_of_edges < num_of_vertices - 1) num_of_edges = num_of_vertices - 1;
     if (num_of_edges > max_edges) num_of_edges = max_edges;
 
+    // initialize random number generator and graph
     mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
     Graph graph;
     
-    // empty neighbors for each vertex at the beginning
+    // initialize (empty) neighbors for each vertex at the beginning
     for (unsigned i = 0; i < num_of_vertices; i++) {
         graph[to_string(i)] = {};
     }
     
+    // make a random order of vertices
     vector<unsigned> order(num_of_vertices);
     iota(order.begin(), order.end(), 0); // fill order vector with 0,1,2,...,num_of_vertices
     shuffle(order.begin(), order.end(), rng); // shuffle vector, something like 5,2,3,7,...
     
-    // connect randomly first, to vertices with lower order
+    // connect vertices randomly first with vertices with lower order
     set<pair<int, int>> seen;
     for (unsigned i = 1; i < num_of_vertices; i++) {
         unsigned u = order[i];
